101987793a52bb883245a38148e172958ef37944
Akash Singh <taryan413@gmail.com>
2023-08-30 12:30:09 +0530

    fix Errors

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable react/prop-types */
-export function TodoItem({ id, title, completed, toggleTodo, deleteTodo })  {
-    return (
-        <li key={id}>
-            <label>
-            <input type='checkbox' checked={completed} onChange={e => toggleTodo(id, e.target.checked)} />
-            {title}
-            </label>
-            <button onClick={() => deleteTodo(id)} className='btn btn-danger' >
-            Delete
-            </button>
-        </li>
-    )
+export function TodoItem({ id, title, completed, toggleTodo, deleteTodo }) {
+  return (
+    <li key={id}>
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      <button onClick={() => deleteTodo(id)} className="btn btn-danger">
+        Delete
+      </button>
+    </li>
+  );
 }
